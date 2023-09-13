073af4141917361430d7dd9cd2bb498614dc77bf
suraj <suraj@MSI>
2023-09-13 13:38:39 +1200

    Add project files.

---
+++ Grade Calculator Program/Grade Calculator.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Grade_Calculator_Program
+{
+    internal class Grade_Calculator
+    {
+        public string name { get; set; }
+        public double mathMarks { get; set; }
+        public double physicsMarks { get; set; }
+        public double chemistryMarks { get; set; }
+        public double computerScienceMarks { get; set; }
+
+        public double average_marks(double mathMarks, double physicsMarks, double chemistryMarks, double computerScienceMarks)
+        {
+            // calculation of average marks.
+            return (mathMarks + physicsMarks + chemistryMarks + computerScienceMarks) / 4;
+        }
+        public string calculate_grade(double averageMarks)
+        {
+            string grade;
+            // find out the grade of the student using the average marks
+            if (averageMarks >= 80)
+            {
+                grade = "Grade A";
+            }
+            else if (averageMarks >= 70 && averageMarks < 80)
+            {
+                grade = "Grade B";
+            }
+            else if (averageMarks >= 60 && averageMarks < 70)
+            {
+                grade = "Grade C";
+            }
+            else if (averageMarks >= 50 && averageMarks < 60)
+            {
+                grade = "Grade D";
+            }
+            else if (averageMarks < 50)
+            {
+                grade = "Grade F";
+            }
+            else
+            {
+                grade = "Invalid";
+            }
+            return grade;
+
+
+
+
+        }
+    }
+}
+
+
+
+    
---
+++ Grade Calculator Program/Program.cs
@@ -0,0 +1,88 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Runtime.Remoting.Messaging;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Grade_Calculator_Program
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Grade_Calculator obj_grade_calculator = new Grade_Calculator();
+            string name;
+           
+
+
+            // Get the marks from user in 4 subjects.
+            Console.WriteLine("Please enter your name");
+            name = Console.ReadLine();
+            Console.WriteLine("Please enter your marks in Math");
+            double mathMarks = Convert.ToDouble(Console.ReadLine());
+            Console.WriteLine("Please enter your marks in Physics");
+            double physicsMarks = Convert.ToDouble(Console.ReadLine());
+            Console.WriteLine("Please enter your marks in Chemistry");
+            double chemistryMarks = Convert.ToDouble(Console.ReadLine());
+            Console.WriteLine("Please enter your marks in Computer Science");
+            double computerScienceMarks = Convert.ToDouble(Console.ReadLine());
+
+            //calculation of average marks
+            //double averageMarks = (mathMarks + physicsMarks + chemistryMarks + computerScienceMarks) / 4;
+
+            double averageMarks = obj_grade_calculator.average_marks(mathMarks, physicsMarks, chemistryMarks, computerScienceMarks);
+            string grade = obj_grade_calculator.calculate_grade(averageMarks);
+
+            try
+            {
+                //applying switch statement.
+                switch (grade)
+                {
+                    case "Grade A":
+                        {
+                            Console.WriteLine("Excellent! Your grade is A");
+                            break;
+                        }
+                    case "Grade B":
+                        {
+                            Console.WriteLine("Good! Your grade is B");
+                            break;
+                        }
+                    case "Grade C":
+                        {
+                            Console.WriteLine("Satisfactory. Your grade is C");
+                            break;
+                        }
+                    case "Grade D":
+                        {
+                            Console.WriteLine("Pass. Your grade is D");
+                            break;
+                        }
+                    case "Grade F":
+                        {
+                            Console.WriteLine("Fail. Your grade is F");
+                            break;
+                        }
+                    default:
+                        {
+                            Console.WriteLine("Invalid");
+                            break;
+                        }
+
+
+                }
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex);
+            }
+            Console.ReadKey();
+        }
+
+        
+        }
+    }
+
+        
